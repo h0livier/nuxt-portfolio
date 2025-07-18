8a2602e8fc7608ee0b008b604a682b9132f4df43
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-07-18 13:13:39 +0200

    added timeline and style

---
+++ components/timeline.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { WorkPlace, Place } from '~/types';
+
+interface TimeLineProps {
+    educations: Place[]
+    experiences: WorkPlace[]
+}
+
+const props = defineProps<TimeLineProps>()
+
+const educations = ref(props.educations)
+const experiences = ref(props.experiences)
+</script>
+<template>
+    <h3>My Journey</h3>
+    <ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
+        <li v-for="experience in experiences">
+            <hr class="bg-blue-500" />
+            <div class="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="timeline-end md:mb-10">
+                <time class="font-mono italic">{{ experience.date }}</time>
+                <div class="text-lg font-black">{{ experience.name }}</div>
+                <div class="text-sm italic mb-1">{{ experience.title }}</div>
+                {{ experience.description }}
+            </div>
+            <hr class="bg-blue-500" />
+        </li>
+        <li v-for="education in educations">
+            <hr class="bg-blue-500" />
+            <div class="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="timeline-end md:mb-10">
+                <time class="font-mono italic">{{ education.date }}</time>
+                <div class="text-lg font-black">{{ education.name }}</div>
+                {{ education.description }}
+            </div>
+            <hr class="bg-blue-500" />
+        </li>
+    </ul>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -12,41 +12,43 @@ const educations: Place[] = [
 
 </script>
 <template>
-    <Cv :experiences="experiences" :educations="educations" />
-    <div class="min-h-screen print:hidden">
-        <p><a href="mailto:olivier.hayot.dev@gmail.com">olivier.hayot.dev@gmail.com</a></p>
-        <div class="hero min-h-[80vh]">
-            <div class="hero-content">
-                <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Hi, I'm Olivier 👋</h1>
-                    <p class="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <PrintButton></PrintButton>
+    <div class="flex justify-center">
+        <Cv :experiences="experiences" :educations="educations" />
+        <div class="min-h-screen print:hidden max-w-[50vw]">
+            <p class="mt-[7.5vh]"><a href="mailto:olivier.hayot.dev@gmail.com">olivier.hayot.dev@gmail.com</a></p>
+            <div class="hero min-h-[80vh]">
+                <div class="hero-content">
+                    <div class="max-w-md">
+                        <h1 class="text-5xl font-bold">Hi, I'm Olivier 👋</h1>
+                        <p class="py-6">
+                            Software Developer with {{ new Date().getFullYear() - new Date(2022, 9, 22).getFullYear() }} years of web experience.
+                        </p>
+                        <PrintButton></PrintButton>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div>
-            <h2>What drives me</h2>
-            <div class="flex">
-                <div class="grow">
-                    <p>Solving Problems</p>
-                    <p>I enjoy building user-friendly and efficient web applications that solve real problems.</p>
-                </div>
-                <div class="grow">
-                    <p>Automation & Creating Tools</p>
-                    <p>Turning small ideas into practical solutions is what keeps me curious and motivated.</p>
+            <div>
+                <h2>What drives me</h2>
+                <div class="flex">
+                    <div class="grow">
+                        <p>Solving Problems</p>
+                        <p>I enjoy building user-friendly and efficient web applications that solve real problems.</p>
+                    </div>
+                    <div class="grow">
+                        <p>Automation & Creating Tools</p>
+                        <p>Turning small ideas into practical solutions is what keeps me curious and motivated.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="mt-10">
-            <h2>Want to get in touch ?</h2>
-            <p>Click on my social links !</p>
-            <p class="flex gap-4">
-                <NuxtLink>github</NuxtLink>
-                <NuxtLink>linkedin</NuxtLink>
-            </p>
+            <Timeline :experiences="experiences" :educations="educations" />
+            <div class="mt-10">
+                <h2>Want to get in touch ?</h2>
+                <p>Click on my social links !</p>
+                <p class="flex gap-4">
+                    <NuxtLink>github</NuxtLink>
+                    <NuxtLink>linkedin</NuxtLink>
+                </p>
+            </div>
         </div>
     </div>
 </template>
