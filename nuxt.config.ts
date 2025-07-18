cd6b01d91f9e891f5385dff7eaff18664702030d
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-07-18 09:45:12 +0200

    adapted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
   css: ["~/assets/app.css"],
   nitro: {
     preset: 'static',
     prerender: {
+      crawlLinks: true,
       routes: [ '/robots.txt']
     }
   },
