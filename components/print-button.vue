0bf090bc02f4e4cc6261436fa06c38a5442a5c6e
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-08-17 18:45:24 +0200

    button text to white

--- components/print-button.vue
+++ components/print-button.vue
@@ -4,5 +4,5 @@ function print(){
 }
 </script>
 <template>
-    <button class="btn btn-primary lg:btn-lg bg-blue-500 border-0 shadow-blue-300" @click="print()">Check out my resume</button>
+    <button class="btn btn-primary lg:btn-lg bg-blue-500 border-0 shadow-blue-300 text-white" @click="print()">Check out my resume</button>
 </template>
