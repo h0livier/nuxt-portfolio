cd550f7c2cd799500e40f7f3e14fef3ad7b2f58a
Olivier <olivier.hayot.dev@gmail.com>
2026-05-25 11:40:08 +0200

    added app insight

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=aac73a8b-510f-4525-843a-e54cbbbe14eb;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=7af01294-4236-42da-ac72-6850cb146164'
+}});
+
+appInsights.loadAppInsights();
+appInsights.startTrackPage('HomePage');
+appInsights.trackPageView({ name: 'HomePage', uri: '/' });
+appInsights.trackTrace({ message: 'Application has started successfully.' });
+
 useHead({
   title: 'Olivier Hayot - Analyst Developer',
   meta: [
