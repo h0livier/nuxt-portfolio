0b289178471db1c9cbe7937f649f9c99537a9e7c
Olivier <olivier.hayot.dev@gmail.com>
2026-08-23 11:47:23 +0200

    spacings

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -86,8 +86,8 @@ async function handleSubmit() {
 }
 </script>
 <template>
-    <div class="card w-full">
-        <div class="card-body">
+    <div class="w-full">
+        <div>
             <div v-if="success" role="alert" class="alert alert-success mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
