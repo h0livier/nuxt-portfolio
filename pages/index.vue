fe0da538e03dc3cbeae7d37b4e242b25b4a9140a
Olivier <olivier.hayot.dev@gmail.com>
2026-08-23 12:07:38 +0200

    spacings

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@ const cvPrintOptions = reactive({
 <template >
     <Cv :experiences="experiences" :educations="educations" :certifications="certifications" :contacts="contacts" :languages="languages" :skills="skills" :variant="cvVariant" :show-missions="cvPrintOptions.showMissions" :show-certifications="cvPrintOptions.showCertifications" :show-about="cvPrintOptions.showAbout" />
     <div data-theme="default" class="flex justify-center bg-base-100">
-        <div class="min-h-screen print:hidden max-w-[90vw] md:max-w-[75vw] lg:max-w-[60vw] xl:max-w-[50vw] xxl:max-w-[40vw]  2xl:max-w-[35vw]">
+        <div class="min-h-screen print:hidden max-w-[90vw] md:max-w-[70vw] lg:max-w-[65vw] xl:max-w-[50vw] xxl:max-w-[40vw]">
             <div class="flex justify-between mt-10 lg:mt-20 mb-10">
                 <p><a href="mailto:olivier.hayot.dev@gmail.com">olivier.hayot.dev@gmail.com</a></p>
                 <LanguageSwitch />
